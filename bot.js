af7b7f9ee18ef1042872e2d686dcd5ed15f7b2ce
SzzakiD <58818938+SzzakiD@users.noreply.github.com>
2019-12-15 16:33:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,11 @@
  * Version 1.2.0
  * Robert Borghese
  */
+const Discord = require('discord.js');
 
+const client = new Discord.Client();
+
+ 
 const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
 
 if(!process.send) {
@@ -16,4 +20,7 @@ process.on('message', function(content) {
 	Files.initBotTest(content);
 });
 
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.y2w__gsuE75P-PVp_i3Zpk-EKl0iRLVl);//BOT_TOKEN is the Client Secret
 }
